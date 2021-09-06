6450c9486c5a4e37b473b2b4f0f22fb6fa08e6b8
Egginhell <80625396+Egginhell@users.noreply.github.com>
2021-09-06 19:01:59 +0900

    app.py 이고잉님 코드 복붙

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+    
+    
 from flask import Flask
 import sqlite3
 app = Flask(__name__)
@@ -5,9 +7,12 @@ app = Flask(__name__)
 def home():
     cnt = sqlite3.connect('topics.db')
     result = cnt.execute('SELECT * FROM topic')
-    
-
-
-
-    return "Hello Flask"
+    topics = result.fetchall()
+    contents = '<h1><a href="/">Web</a></h1>'
+    contents += '<ol>'
+    for id, title,body in topics: 
+        contents += '<li><a href="/read/'+str(id)+'">'+title+'</a></li>'    
+    contents += '</ol>'
+    contents +='<h2>Welcome</h2>Hello, WEB'
+    return contents
 app.run(debug=True)
